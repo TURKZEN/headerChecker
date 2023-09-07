9a700d03cf01b41c3f5c7bd25e0163b18df01679
TURKZEN <batuhanthd@gmail.com>
2023-09-07 17:52:01 +0300

    Font Changed,Output edited(screenshots)

--- headerChecker.py
+++ headerChecker.py
@@ -57,55 +57,58 @@ def Main():
         X_XSS_Protection = req.headers.get("X-XSS-Protection")
         Content_Security_Policy = req.headers.get("Content-Security-Policy")
         X_Content_Type_Options= req.headers.get("X-Content-Type-Options")
-        
+        statusCode = req.status_code
 
         Non_Headers = []
 
-        font = ImageFont.truetype("Roboto-Black.ttf", size=50)
+        font = ImageFont.truetype("EleganTech.ttf", size=50)
         im = Image.new("RGBA",(1920,1080),("Black"))
 
         draw = ImageDraw.Draw(im)
-        draw.text( (0,0),"URL : {}".format(URL),fill="White",font=font)
+        draw.text( (20,0),"GET ",fill="White",font=font)
+        draw.text( (150,0),URL,fill="Cyan",font=font)
 
-        draw.text( (0,50),"")
+        draw.text( (20,100),"Status Code : ",fill="White",font=font)
+        if statusCode <= 399:
+            statusCode = str(statusCode)
+            draw.text( (400,100),statusCode,fill="Green",font=font)
+        else:
+            statusCode = str(statusCode)
+            draw.text( (400,100),statusCode,fill="Red",font=font)
+
+        draw.text( (50,200),"Response Headers",fill="White",font=font)
+        draw.text( (50,240),"---------------------",fill="White",font=font)
 
         if Strict_Transport_Security == None:
             Non_Headers.append("Strict-Transport-Security")
-            draw.text( (0,100), "Strict-Transport-Security : {}".format(Strict_Transport_Security),fill="Red",font=font)
+            draw.text( (50,300), "Strict-Transport-Security : {}".format(Strict_Transport_Security),fill="Red",font=font)
         else:
-            draw.text( (0,100), "Strict-Transport-Security : {}".format(Strict_Transport_Security),fill="Green",font=font)
+            draw.text( (50,300), "Strict-Transport-Security : {}".format(Strict_Transport_Security),fill="Green",font=font)
         
-        draw.text( (0,150),"")
 
         if X_Frame_Options == None:
             Non_Headers.append("X-Frame-Options")
-            draw.text( (0,200), "X-Frame-Options : {}".format(X_Frame_Options),fill="Red",font=font)
+            draw.text( (50,400), "X-Frame-Options : {}".format(X_Frame_Options),fill="Red",font=font)
         else:
-            draw.text( (0,200), "X-Frame-Options : {}".format(X_Frame_Options),fill="Green",font=font)
-        
-        draw.text( (0,250),"")
+            draw.text( (50,400), "X-Frame-Options : {}".format(X_Frame_Options),fill="Green",font=font)
 
         if X_XSS_Protection == None:
             Non_Headers.append("X-XSS-Protection")
-            draw.text( (0,300),"X-XSS-Protection : {}".format(X_XSS_Protection),fill="Red",font=font)
+            draw.text( (50,500),"X-XSS-Protection : {}".format(X_XSS_Protection),fill="Red",font=font)
         else:
-            draw.text( (0,300),"X-XSS-Protection : {}".format(X_XSS_Protection),fill="Green",font=font)
-
-        draw.text( (0,350),"")
+            draw.text( (50,500),"X-XSS-Protection : {}".format(X_XSS_Protection),fill="Green",font=font)
 
         if Content_Security_Policy == None:
             Non_Headers.append("Content-Security-Policy")
-            draw.text( (0,400),"Content-Security-Policy : {}".format(Content_Security_Policy),fill="Red",font=font)
+            draw.text( (50,600),"Content-Security-Policy : {}".format(Content_Security_Policy),fill="Red",font=font)
         else:
-            draw.text( (0,400),"Content-Security-Policy : {}".format(Content_Security_Policy),fill="Green",font=font)
-
-        draw.text( (0,450),"")
+            draw.text( (50,600),"Content-Security-Policy : {}".format(Content_Security_Policy),fill="Green",font=font)
 
         if X_Content_Type_Options == None:
             Non_Headers.append("X-Content-Type-Options")
-            draw.text( (0,500),"X-Content-Type-Options : {}".format(X_Content_Type_Options),fill="Red",font=font)
+            draw.text( (50,700),"X-Content-Type-Options : {}".format(X_Content_Type_Options),fill="Red",font=font)
         else:
-            draw.text( (0,500),"X-Content-Type-Options : {}".format(X_Content_Type_Options),fill="Green",font=font)
+            draw.text( (50,700),"X-Content-Type-Options : {}".format(X_Content_Type_Options),fill="Green",font=font)
 
         del draw
 
